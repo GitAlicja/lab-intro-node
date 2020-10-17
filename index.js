de7d3d20dc5c96b561fc7abff0218c93dc833b18
Alicja Slupska <ala.slupska@gmail.com>
2020-10-17 16:12:01 +0200

    LAB done (including bonus)

--- index.js
+++ index.js
@@ -1,17 +1,73 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.length = 0;
+    this.items = [];
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+    if (pos <= this.items.length - 1) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
 
-  avg() {}
+    if (this.items.length > 0) {
+      return this.items[this.items.length - 1];
+    } else {
+      throw new Error('EmptySortedList');
+    }
+
+  }
+
+  min() {
+
+    if (this.items.length > 0) {
+      return this.items[0];
+    } else {
+      throw new Error('EmptySortedList');
+    }
+
+  }
+
+  sum() {
+
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      let total = 0;
+      this.items.forEach(item => {
+        total += item;
+      });
+      return total;
+
+    }
+  }
+
+  avg() {
+
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      let total = 0;
+      this.items.forEach(item => {
+        total += item;
+      });
+      return total / this.items.length;
+
+    }
+  }
+
 }
 
 module.exports = SortedList;
